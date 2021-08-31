86dae8db7449504ae51b7b11f0b98e1dd625a01c
AJC-PR, LLC <26610312+AJC-PR2021@users.noreply.github.com>
2021-08-30 21:09:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/AmesIowaHousingPricesDashboard/LotDashboard?:language=en-US&:retry=yes&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/AmesIowaHousingPricesDashboard/LotDashboard?:language=en-US&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
